9f5c6e8a29883ada65b376050a5641561e2c35e0
EdisonP-9814 <edip9814@gmail.com>
2024-10-25 15:31:07 -0500

    Rutas de Vue implementacion

---
+++ router/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello App!</h1>
+    <p>
+      <strong>Current route path:</strong> {{ $route.fullPath }}
+    </p>
+    <nav>
+      <RouterLink to="/">
+          <button>Go to Home</button>
+      </RouterLink>
+      <RouterLink to="/about">
+        <button>to About</button>
+      </RouterLink>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </template>
---
+++ router/src/Components/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>HomeView</h2>
+    <button @click="gotoAbout">Go to About</button>
+  </template>
+
+
+<script lang="ts">
+export default {
+    methods: {
+        gotoAbout() {
+            this.$router.push('/about');
+        }
+    }
+}
+</script>
+
+
+
